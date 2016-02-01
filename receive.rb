2d179723c48de1c695da14c2d14d320d05357337
Prasad Surase <prasadsurase@gmail.com>
2016-02-01 15:56:37 +0530

    Added code for first tutorial

---
+++ receive.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bunny"
+
+conn = Bunny.new
+conn.start
+
+channel = conn.create_channel
+queue = channel.queue('hello')
+puts " [*] Waiting for messages in #{queue.name}. To exit press CTRL+C"
+queue.subscribe(:block => true) do |delivery_info, properties, body|
+  puts " [x] Received #{body}"
+
+  # cancel the consumer to exit
+  delivery_info.consumer.cancel
+end
+#conn.close
